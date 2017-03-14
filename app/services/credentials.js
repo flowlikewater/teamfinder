27c27c576a7ef490979b3dd74902ece91e00043f
kenNg1 <kng1230@hotmail.com>
2017-03-14 20:09:09 +0800

    added add-team-member component

--- app/services/credentials.js
+++ app/services/credentials.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  player: null,
-  name: null,
+  player: false,
+  name: "Joe",
+  id: "0",
   addPlayer(player){
     this.set('name', player.get('name'));
     debugger;
