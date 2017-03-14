4f5cccfe15735ff48da0fba951c54fd569e9591b
Kevin <cho.kevin@hotmail.com>
2017-03-14 10:28:10 +0800

    added functionality: send message to team

--- app/routes/team.js
+++ app/routes/team.js
@@ -3,7 +3,30 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return Ember.RSVP.hash({
-      teams: this.store.findRecord('team', params.team_id)
+      teams: this.store.findRecord('team', params.team_id),
+      players: this.store.findAll('player'),
+      basketball: this.store.query('team', {
+        orderBy: 'category',
+        equalTo: 'basketball'
+      }),
+      gym: this.store.query('team', {
+        orderBy: 'category',
+        equalTo: 'gym'
+      }),
+      football: this.store.query('team', {
+        orderBy: 'category',
+        equalTo: 'football'
+      })
     });
+  },
+  actions: {
+    sendMessage(params) {
+      var newMessage = this.store.createRecord('message',params);
+      var team = params.team;
+      team.get('messages').addObject(newMessage);
+      newMessage.save().then(function(){
+        return team.save();
+      })
+    }
   }
 });
